74ba8ddb43586a2fd1c08c0b71777f372bfa1386
Raluca Lapusneanu <raluca.lapusneanu@gmail.com>
2016-10-21 10:55:02 +0300

    add view models and controls

---
+++ PizzaManagement/PizzaManagement/ViewModels/ManufacturerViewModel.cs
@@ -0,0 +1,29 @@
+﻿//  --------------------------------------------------------------------------------------------
+//  <Copyright>
+//      Copyright © 2004 - 2016 Stabiplan bv. / Stabiplan International bv. All rights reserved.
+//  </Copyright>
+//  --------------------------------------------------------------------------------------------
+
+using System.ComponentModel;
+
+
+namespace PizzaManagement.ViewModels
+{
+   public class ManufacturerViewModel : INotifyPropertyChanged
+   {
+      #region Interface Members
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      #endregion
+
+      #region Methods - Protected
+
+      protected virtual void RaisePropertyChanged( string propertyName )
+      {
+         PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+      }
+
+      #endregion
+   }
+}
